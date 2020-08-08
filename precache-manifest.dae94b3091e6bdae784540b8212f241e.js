284b33c79e88a69de211ae9e8a3f9a123f14e03b
Nnamdi Osuagwu <kelvinosuagwu@Nnamdis-MacBook-Air.local>
2020-08-08 02:33:07 +0100

    Updates

---
+++ precache-manifest.dae94b3091e6bdae784540b8212f241e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a6d9b65b35c9872196bf3e51e1bcca6",
+    "url": "/RobotsFriendsApp/index.html"
+  },
+  {
+    "revision": "6d0a747418248d47f052",
+    "url": "/RobotsFriendsApp/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3bd5009d312a99ba621f",
+    "url": "/RobotsFriendsApp/static/css/main.93a8ab1e.chunk.css"
+  },
+  {
+    "revision": "6d0a747418248d47f052",
+    "url": "/RobotsFriendsApp/static/js/2.eb9e89e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RobotsFriendsApp/static/js/2.eb9e89e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bd5009d312a99ba621f",
+    "url": "/RobotsFriendsApp/static/js/main.55f9a8f2.chunk.js"
+  },
+  {
+    "revision": "75011f8f9f583c8e67d9",
+    "url": "/RobotsFriendsApp/static/js/runtime-main.deb276b9.js"
+  }
+]);
